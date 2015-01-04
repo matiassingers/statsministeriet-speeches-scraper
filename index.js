d7b4687a0f69d1e82e1f2c95f227f1e2f982056b
Matias Singers <mail@matiassingers.com>
2015-01-04 01:39:02 +0100

    Add logic to index.js for scraping and formatting of each speech

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 'use strict';
 
-module.exports = function(){
+var got = require('got');
+var cheerio = require('cheerio');
+var md = require('html-md');
 
+module.exports = function(id, callback) {
+  var url = 'http://www.stm.dk/_p_' + id + '.html';
+
+  got(url, function(err, data){
+    var $ = cheerio.load(data);
+
+    var meta = $('meta[name="created"]');
+    var speech = $('.maininner.maininner-page');
+
+    speech.find('h1').remove();
+    speech.find('.nedtonet').remove();
+
+    var linkElement = speech.find('a[onclick]');
+    var link = linkElement.attr('onclick').split('\'')[1];
+    linkElement.attr('href', link);
+
+    speech.children().last().remove();
+    speech.children().last().remove();
+    speech.children().last().remove();
+
+    callback(null, {
+      source: url,
+      video: link,
+      date: meta.attr('content'),
+      html: speech.html(),
+      markdown: md(speech.html(), {inline: true}).replace(/\\/gi, '')
+    })
+  });
 };
